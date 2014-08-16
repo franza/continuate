7a8ec18ef5466c60d8fab366ccc424b721c2cbbc
franza <nico.franza@gmail.com>
2014-08-17 00:07:32 +0400

    Refactored callback extracting

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ module.exports = function (fun) {
    * @param {Function} callback
    */
   return function () {
-    var args = Array.prototype.slice.call(arguments, 0, -1);
-    var callback = arguments[arguments.length - 1];
+    var args = Array.prototype.slice.call(arguments);
+    var callback = args.pop();
     var result;
     try {
       result = fun.apply(this, args);
